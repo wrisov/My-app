5263006a0ae7f1c715a98bbf014de86d1d88a004
Wrisov Karmakar <wrisovkarmakar@gmail.com>
2019-12-20 01:27:13 +0530

    Products added with authentication and wall of products according to time created

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
-    
+    mount_uploader :image, ImageUploader
+    serialize :image, JSON
+    belongs_to :user, optional: true
+
+    validates :brand, :price, :model, presence: true
 end
